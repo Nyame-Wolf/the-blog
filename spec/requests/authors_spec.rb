5d37c81e27d5459ec232ddebc3018afa0d210559
Nyame-Wolf <mumenyam@gmail.com>
2022-10-25 20:24:29 +0300

    add author controller tests

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -2,16 +2,46 @@ require 'rails_helper'
 
 RSpec.describe 'Authors', type: :request do
   describe 'GET /show' do
-    it 'returns http success' do
+    before(:each) do
       get '/authors/show'
+    end
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+    it 'renders correct template text' do
+      expect(response.body).to include('Authors show')
     end
   end
 
   describe 'GET /index' do
-    it 'returns http success' do
+    before(:each) do
       get '/authors/index'
+    end
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET /' do
+    before(:each) do
+      get '/'
+    end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'renders correct template text' do
+      expect(response.body).to include('Authors')
     end
   end
 end
