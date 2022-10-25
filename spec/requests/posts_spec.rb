ac61b7b91704806c3ef34e173c76a1821f8e0145
Nyame-Wolf <mumenyam@gmail.com>
2022-10-25 20:24:52 +0300

    add post controller tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:each) do
+      get '/authors/:author_id/posts/'
+    end
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'renders correct template text' do
+      expect(response.body).to include('User posts')
     end
   end
 
   describe 'GET /show' do
+    before(:each) do
+      get '/authors/:author_id/posts/:id'
+    end
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+    it 'renders correct template text' do
+      expect(response.body).to include('posts show route')
     end
   end
 end
