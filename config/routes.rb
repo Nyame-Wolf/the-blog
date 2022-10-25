fb5eb09e95da4f4bb4133096a47735f3a841d301
Nyame-Wolf <mumenyam@gmail.com>
2022-10-25 20:23:09 +0300

    set up controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'authors/show'
+ root 'authors#index'
   get 'authors/index'
-  root 'authors#index'
   resources :authors, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
     end
   end
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'users/show'
-  # get 'users/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
