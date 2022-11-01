be11bce7dafc88986df57bb01504576a127b61bf
Nyame-Wolf <mumenyam@gmail.com>
2022-11-01 19:30:02 +0300

    fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'Posts', type: :request do
       @author1 = Author.create!(name: 'Vitor',
                                 photo: 'https://i.picsum.photos/id/908/200/200.jpg?hmac=CovMVsq4EkU03tnOxNLyxYsLlemPPHBizxcnmaHaRcU',
                                 bio: 'Teacher from Brazil.')
-      post1 = Post.create!(author: @author1, title: 'Post 1', text: 'text Post 1')                         
+      post1 = Post.create!(author: @author1, title: 'Post 1', text: 'text Post 1')
     end
     before(:each) do
       get author_posts_path(@author1)
     end
-   
+
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
