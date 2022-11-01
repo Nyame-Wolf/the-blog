2cc33216a4a0442cb67c5f3688760006e885a283
Nyame-Wolf <mumenyam@gmail.com>
2022-11-01 19:34:17 +0300

    fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     before(:all) do
-      @author1 = Author.create!(name: 'Vitor',
-                                photo: 'https://i.picsum.photos/id/908/200/200.jpg?hmac=CovMVsq4EkU03tnOxNLyxYsLlemPPHBizxcnmaHaRcU',
-                                bio: 'Teacher from Brazil.')
-      post1 = Post.create!(author: @author1, title: 'Post 1', text: 'text Post 1')
+      # @author1 = Author.create!(name: 'Vitor',
+      #                           photo: 'https://i.picsum.photos/id/908/200/200.jpg?hmac=CovMVsq4EkU03tnOxNLyxYsLlemPPHBizxcnmaHaRcU',
+      #                           bio: 'Teacher from Brazil.')
+      # post1 = Post.create!(author: @author1, title: 'Post 1', text: 'text Post 1')
     end
     before(:each) do
       get author_posts_path(@author1)
