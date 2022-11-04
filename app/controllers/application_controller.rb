84fc7db86c185f67d9f337c88178930b7b977660
Nyame-Wolf <mumenyam@gmail.com>
2022-11-04 20:41:31 +0300

    Fix ability to stop using default current_user to use current_author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_author!
 
+  # to change fro default current_user to current_author
+  def current_ability
+    @current_ability ||= Ability.new(current_author)
+  end
+
   protected
 
   def configure_permitted_parameters
