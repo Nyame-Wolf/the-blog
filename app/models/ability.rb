d1150de3a35f6b675ed5bbec660f4d2549e2605e
Nyame-Wolf <mumenyam@gmail.com>
2022-11-04 20:39:41 +0300

    Add cancancan

---
+++ app/models/ability.rb
@@ -0,0 +1,41 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(author)
+    can :read, Post
+
+    return unless author.present? # additional permissions for logged in users (they can read their own posts)
+
+    can :manage, Post, author: author
+    can :manage, Comment, author: author
+    can :manage, Like, author: author
+
+    return unless author.role == 'admin' # additional permissions for administrators
+
+    can :manage, :all
+    # Define abilities for the user here. For example:
+    #
+    #   return unless user.present?
+    #   can :read, :all
+    #   return unless user.admin?
+    #   can :manage, :all
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, published: true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  end
+end
